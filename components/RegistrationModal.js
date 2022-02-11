3df89927221dc5b64281a3fbc995aedd7938e449
kinb98 <kinjalkbajpai@gmail.com>
2022-02-11 22:13:34 +0530

    Sending registration data to server

--- components/RegistrationModal.js
+++ components/RegistrationModal.js
@@ -1,22 +1,42 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import axios from "axios";
 
 const RegistrationModal = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const submit = async () => {
+    const response = await axios.post("/api/auth/register", {
+      email,
+      password,
+      passwordConfirmation,
+    });
+    console.log(response);
+  };
+
   return (
     <Fragment>
       <h2>Sign up</h2>
       <div>
-        <form
-          onSubmit={(e) => {
-            alert("Log in");
-            e.preventDefault();
-          }}
-        >
-          <input id="email" type="email" placeholder="Email address" />
-          <input id="password" type="password" placeholder="Password" />
+        <form onSubmit={submit}>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <input
             id="passwordconfirmation"
             type="password"
             placeholder="Enter password again"
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
           />
           <button>Sign up</button>
         </form>
